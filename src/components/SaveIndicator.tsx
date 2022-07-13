280c9c40c5371da3da623d0216c46309b001e56f
Ibrahim Dembele <71393282+idembele70@users.noreply.github.com>
2022-07-13 15:04:36 +0200

    [saveIndicator]
    Done so easily

--- src/components/SaveIndicator.tsx
+++ src/components/SaveIndicator.tsx
@@ -1,6 +1,6 @@
-import { useObservable, useObservableCallback, useObservableState } from 'observable-hooks';
-import React from 'react';
-import { concat, debounceTime, delay, distinctUntilChanged, EMPTY, filter, map, merge, of, pluck, share, switchAll, tap, timer } from 'rxjs';
+import React from 'react'
+import { pluckCurrentTargetValue, useObservable, useObservableCallback, useObservableState } from 'observable-hooks';
+import { concat, debounceTime, delay, distinctUntilChanged, filter, map, merge, of, pluck, share, switchAll, tap } from 'rxjs';
 import styled from 'styled-components';
 
 const Title = styled.h1``;
@@ -28,8 +28,36 @@ const SaveInfo = styled.h4`
   margin-top: 5px;
 `;
 const SaveIndicator = () => {
+  const [handleChange, change$] = useObservableCallback(e$=>e$.pipe(
+    debounceTime(500),
+    pluck("target","value"),
+    distinctUntilChanged(),
+    share()
+  ));
+  const [isSaving, setIsSaving] = React.useState(false);
+  const saving$ = useObservable(()=> change$.pipe(
+    map(()=>of("saving")),
+    tap(()=>setIsSaving(true)),
+    
+  ))
+  const saved$ = useObservable(()=>change$.pipe(
+    tap(()=>setIsSaving(false)),
+    filter(()=> !isSaving),
+    map(()=>concat(
+      of("Saved").pipe(delay(1500)),
+      of(`Last updated: ${new Date().toLocaleString()}`).pipe(delay(1000)),
+    ))
+  ))
+  const saveStatus$ = useObservable(()=>merge(saving$,saved$).pipe(
+    switchAll()
+  ))
   return (
     <>
+    <Title>Take a note!</Title>
+    <InputContainer>
+    <Input onChange={handleChange}/>
+    <SaveInfo>{useObservableState(saveStatus$,"All changes saved")}</SaveInfo>
+    </InputContainer>
     </>
   )
 }
