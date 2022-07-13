14dd940691bc00f4eac7e7232b313463da0d8722
Ibrahim Dembele <71393282+idembele70@users.noreply.github.com>
2022-07-14 01:11:14 +0200

    [TypeAhead] Done

--- src/components/SmartCounter.tsx
+++ src/components/SmartCounter.tsx
@@ -1,6 +1,3 @@
-import { useObservable, useObservableState } from 'observable-hooks';
-import React from "react";
-import { map, scan, startWith, switchMap, takeWhile, tap, timer } from 'rxjs';
 import styled from 'styled-components';
 const Input = styled.input`
   height: 20px;
@@ -13,30 +10,8 @@ width: 115px;
   text-align: center;
 `;
 const SmartCounter = () => {
-  const [endCount, setEndCount] = React.useState(0);
-  const [startCount, setStartCount] = React.useState(0);
-  const handleChange = (e:React.KeyboardEvent<HTMLInputElement>) => { 
-    if(e.code === "Enter")
-    setEndCount(e.currentTarget.valueAsNumber)
-   }
-   const count$ = useObservable((input$)=>input$.pipe(
-    switchMap(([startCount,endCount])=>timer(0).pipe(
-      map(()=>startCount < endCount ? 1 : -1),
-      startWith(startCount),
-      scan((acc,cur)=>acc + cur),
-      takeWhile((v)=>{
-        if(startCount > endCount)
-        return v > endCount
-        else
-        return v < endCount
-      }),
-      tap((v)=>setStartCount(v)),
-    ))
-   ) ,[startCount,endCount])
   return (
     <>
-    <Input type="number" onKeyUp={handleChange}/>
-    <Counter>{useObservableState(count$,0)}</Counter>
     </>
   )
 }
--- src/components/TypeAhead.tsx
+++ src/components/TypeAhead.tsx
@@ -1,8 +1,156 @@
+import { useObservable, useObservableCallback, useSubscription } from 'observable-hooks';
 import React from 'react'
+import { debounceTime, distinctUntilChanged, EMPTY, filter, fromEvent, map, pluck, switchMap, tap, timer } from 'rxjs';
+import styled from 'styled-components';
 
+const Container = styled.div`
+  padding: 15px;
+`;
+
+const Title = styled.h5`
+  display:inline;
+  margin-right: 15px;
+`;
+const Input = styled.input`
+  height:25px;
+  width: 180px;
+  `;
+  const Hr = styled.hr`
+    margin: 15px 0;
+    max-width: 50%;
+  `;
+  const ContinentList = styled.ul`
+    list-style-type: none;
+    padding: 0;
+  `;
+  const Continent = styled.li`
+    
+  `;
+  const Loader = styled.div`
+    
+  `;
 const TypeAhead = () => {
+  const continentsDB = React.useMemo(() => [
+    "africa",
+    "antartica",
+    "asia",
+    "australia",
+    "europe",
+    "north america",
+    "south america",
+    "africa",
+    "antartica",
+    "asia",
+    "australia",
+    "europe",
+    "north america",
+    "south america",
+    "africa",
+    "antartica",
+    "asia",
+    "australia",
+    "europe",
+    "north america",
+    "south america",
+    "africa",
+    "antartica",
+    "asia",
+    "australia",
+    "europe",
+    "north america",
+    "south america",
+    "africa",
+    "antartica",
+    "asia",
+    "australia",
+    "europe",
+    "north america",
+    "south america",
+    "africa",
+    "antartica",
+    "asia",
+    "australia",
+    "europe",
+    "north america",
+    "south america",
+    "africa",
+    "antartica",
+    "asia",
+    "australia",
+    "europe",
+    "north america",
+    "south america",
+    "africa",
+    "antartica",
+    "asia",
+    "australia",
+    "europe",
+    "north america",
+    "south america",
+    "africa",
+    "antartica",
+    "asia",
+    "australia",
+    "europe",
+    "north america",
+    "south america",
+    "africa",
+    "antartica",
+    "asia",
+    "australia",
+    "europe",
+    "north america",
+    "south america",
+  ], [])
+  const [searchDB, setSearchDB] = React.useState<string[]>([]);
+  const [continents, setContinents] = React.useState<string[]>([]);
+  const [loading, setLoading] = React.useState(false);
+  const [handleChange, change$] = useObservableCallback(e$=>e$.pipe(
+    debounceTime(300),
+    pluck("target","value"),
+    distinctUntilChanged(),
+    tap(()=>setContinents([])),
+    filter((searchTerm:any)=>searchTerm?.length > 0),
+    tap(()=>setLoading(true)),
+    switchMap((searchTerm)=>timer(750).pipe(
+      tap(()=>{
+        const searchedContinents:string[] = continentsDB.filter(
+        continent=>continent.includes(searchTerm as string)
+        )
+        setSearchDB(searchedContinents)
+        setContinents(searchedContinents.slice(0,44))
+        setLoading(false)
+      })
+    ))
+  ));
+  const scroll$ = useObservable(()=>fromEvent(document,"scroll").pipe(
+    map(()=>{
+      const {clientHeight,scrollHeight,scrollTop} = document.documentElement
+      const scrollMaxHeight = scrollHeight - clientHeight
+      const scrollPostion = Math.round(scrollTop)
+      if(scrollMaxHeight <= scrollPostion){
+        return true
+      }
+      return false
+    })
+  ))
+  useSubscription(change$)
+  useSubscription(scroll$,(v)=> {
+    if(v){
+      setContinents(searchDB.slice(0,continents.length + 10))
+    }
+  })
   return (
-    <div>TypeAhead</div>
+    <Container>
+      <Title>Get continents</Title>
+      <Input onChange={handleChange}/>
+      <Hr/>
+      <ContinentList>
+      {
+      loading ? <Loader>Loading ...</Loader> : 
+      continents.map((continent,idx)=><Continent key={idx}>{continent}</Continent>)}
+      </ContinentList>
+    </Container>
   )
 }
 
